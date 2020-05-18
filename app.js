b34abd233e823be2f278b12cacd0131c80b04ff5
Sirag Ahmed <ajj.siraj@gmail.com>
2020-05-19 01:58:51 +0200

    cors configuration

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const passport = require("passport");
-const config = require("./config");
+// const config = require("./config");
 const cors = require("cors");
 
 const indexRouter = require("./routes/index");
@@ -17,6 +17,24 @@ const moviesRouter = require("./routes/movies");
 const searchRouter = require("./routes/search");
 
 const app = express();
+
+//configure cors
+let whitelist = ["http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      if (!origin) return callback(null, true);
+      if (whitelist.indexOf(origin) === -1) {
+        var message =
+          "The CORS policy for this origin doesn't allow access from the particular origin.";
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 //mongodb connection
 const db = mongoose.connect(process.env.DB_STRING, {

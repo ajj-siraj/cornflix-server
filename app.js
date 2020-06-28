0192f4709cd5e379e80e59a9261bc6c70cea3877
Sirag Ahmed <ajj.siraj@gmail.com>
2020-06-28 03:16:01 +0200

    cors whitelist

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(function (req, res, next) {
 app.use(express.static(path.join(__dirname, "public")));
 
 //configure cors
-let whitelist = ["http://localhost:3000", "http://localhost:4000", "http://localhost:5000"];
+let whitelist = ["http://localhost:3000", "http://localhost:4000", "http://localhost:5000", "http://cornflix.herokuapp.com/"];
 
 app.use(
   cors({
